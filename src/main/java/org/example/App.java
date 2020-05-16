e9caf9aa926375f77cd6448ea9d8bcc80ef95a6a
Jane-Law <jane_law_lj@163.com>
2020-05-16 13:54:32 +0800

    test444

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hi World IIIkkk   ~!" );
         System.out.println( "Hi World luojiao!" );
+        System.out.println( "Hi World luojiao!" );
     }
 }
